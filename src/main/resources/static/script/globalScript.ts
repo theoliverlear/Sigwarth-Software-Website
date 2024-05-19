75895a46b00be389893611bdd84c22e8fe33847f
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-05-19 11:57:27 -0500

    Add globalScript.ts to handle logic for hamburger menu and to disable image dragging

---
+++ src/main/resources/static/script/globalScript.ts
@@ -0,0 +1,20 @@
+// Disable all img from being dragged
+const images: HTMLCollectionOf<HTMLImageElement> = document.getElementsByTagName('img');
+for (let image of Array.from(images)) {
+    image.draggable = false;
+}
+
+const hamburgerListItems: HTMLCollectionOf<Element> = document.getElementsByClassName('hamburger-list-items');
+const hamburgerButton: HTMLElement | null = document.getElementById('hamburger-menu-icon');
+
+function toggleListItemsView(): void {
+    Array.from(hamburgerListItems).forEach((element: Element) => {
+        if ((<HTMLElement>element).style.display === 'none') {
+            (<HTMLElement>element).style.display = 'block';
+        } else {
+            (<HTMLElement>element).style.display = 'none';
+        }
+    });
+}
+
+hamburgerButton.addEventListener('click', toggleListItemsView);
