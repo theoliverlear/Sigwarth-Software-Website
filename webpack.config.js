1bcb6d195f10bcc3f16ca13c265b02a8a43cda4c
theoliverlear <sigwartholiver2@gmail.com>
2024-05-20 21:30:32 -0500

    Refactor webpack.config.js to include new entry points, add Babel loader, and include jQuery via ProvidePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/main/resources/static/script/globalScript.ts',
+    entry: [
+        './src/main/resources/static/script/globalScript.ts',
+        './src/main/resources/static/script/homeScript.ts'
+    ],
     module: {
         rules: [
             {
-                test: /\.ts?$/, // This will apply the loader to TypeScript files
-                use: 'ts-loader',
+                test: /\.ts$/,
                 exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader', // Add Babel Loader
+                        options: {
+                            presets: [
+                                '@babel/preset-env' // Use preset-env for all necessary transformations
+                            ]
+                        }
+                    },
+                    'ts-loader' // Keep using ts-loader for TypeScript transpilation
+                ]
             },
         ],
     },
     resolve: {
-        extensions: ['.ts', '.js'], // recognize both JavaScript and TypeScript files
+        extensions: [
+            '.ts',
+            '.js'
+        ],
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './src/main/resources/static/script'),
     },
-    mode: 'development'
+    mode: 'development',
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
 };
