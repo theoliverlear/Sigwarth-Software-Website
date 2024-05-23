c11404580d3786bdb7151ff8dbcd6f94f6633abc
theoliverlear <sigwartholiver2@gmail.com>
2024-05-22 21:18:13 -0500

    Add home.html page functionalities in homeScript.ts for text typing animation

---
+++ src/main/resources/static/script/homeScript.ts
@@ -0,0 +1,40 @@
+import './homeCarouselScript';
+let body = $('body');
+const screenWindow: JQuery<Window> = $(window);
+const softwareDevelopmentText: string = "Software Development";
+const softwareDevelopmentTyped: JQuery<HTMLElement> = $('#software-development-typed');
+const webDevelopmentText: string = "Web Development";
+const leadershipText: string = "Leadership";
+const leadershipTyped: JQuery<HTMLElement> = $('#leadership-typed');
+const webDevelopmentTyped: JQuery<HTMLElement> = $('#web-development-typed');
+
+function typeText(textToType: string, elementToType: JQuery<HTMLElement>): Promise<void> {
+    let currentCharIndex: number = 0; // Moved local index to function scope.
+    function typeChar(): Promise<void> {
+        return new Promise((resolve) => {
+            if (currentCharIndex < textToType.length) {
+                elementToType.append(textToType.charAt(currentCharIndex));
+                currentCharIndex++;
+                setTimeout(() => resolve(typeChar()), 150);
+            } else {
+                resolve(); // Resolves when typing is done.
+            }
+        });
+    }
+    return typeChar(); // Kick off recursive typing and return promise.
+}
+
+$(document).ready(function (): void {
+    typeText(softwareDevelopmentText, softwareDevelopmentTyped).then(function(): void {
+        typeText(webDevelopmentText, webDevelopmentTyped).then(function(): void {
+            typeText(leadershipText, leadershipTyped);
+        });
+    });
+});
+function setScreenLayout(): void {
+    if (screenWindow.innerWidth() <= 920) {
+        body.toggleClass('mobile-view');
+    }
+}
+setScreenLayout();
+screenWindow.on('resize', setScreenLayout);
