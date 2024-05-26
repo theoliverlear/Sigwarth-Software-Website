4f75a7802422704705d793de197aa5f5ff802fa4
theoliverlear <sigwartholiver2@gmail.com>
2024-05-26 11:23:41 -0500

    Add SecurityConfig.java for creating a security filter chain that permits all requests and disables CSRF

---
+++ src/main/java/org/theoliverlear/sigwarthsoftware/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.theoliverlear.sigwarthsoftware.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    //------------------------Security-Filter-Chain---------------------------
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // HttpSecurity is the builder object used to create the security
+        // filter chain. We authorize all requests and disable CSRF.
+        http.authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .csrf()
+                .disable();
+        return http.build();
+    }
+}
