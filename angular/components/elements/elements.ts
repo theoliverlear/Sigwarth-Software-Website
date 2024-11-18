633388ac867dc22e076d16a881ee83f2334b1d88
theoliverlear <sigwartholiver2@gmail.com>
2024-11-17 20:17:13 -0600

    Add elements.ts to bundle components to inject into the Angular app

---
+++ angular/components/elements/elements.ts
@@ -0,0 +1,19 @@
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {SsAnchorComponent} from "./ss-anchor/ss-anchor.component";
+import {SsButtonComponent} from "./ss-button/ss-button.component";
+import {SsCodeComponent} from "./ss-code/ss-code.component";
+import {SsFooterComponent} from "./ss-footer/ss-footer.component";
+import {SsHeadComponent} from "./ss-head/ss-head.component";
+import {SsImgComponent} from "./ss-img/ss-img.component";
+import {TypingTextComponent} from "./typing-text/typing-text.component";
+
+export const elements: any[] = [
+    NavBarComponent,
+    SsAnchorComponent,
+    SsButtonComponent,
+    SsCodeComponent,
+    SsFooterComponent,
+    SsHeadComponent,
+    SsImgComponent,
+    TypingTextComponent
+]
