435eca0683431eca468f5670a25d03945258ccac
theoliverlear <sigwartholiver2@gmail.com>
2024-11-17 20:50:51 -0600

    Add ss-button.component.ts for custom button functionality in the Angular application

---
+++ angular/components/elements/ss-button/ss-button.component.ts
@@ -0,0 +1,38 @@
+import {Component, HostBinding, Input, OnInit} from "@angular/core";
+import {ElementSize} from "../../../models/ElementSize";
+import {ButtonText} from "./models/ButtonText";
+import {ButtonPosition} from "./models/ButtonPosition";
+
+@Component({
+    selector: 'ss-button',
+    templateUrl: './ss-button.component.html',
+    styleUrls: ['./ss-button-style.component.css']
+})
+export class SsButtonComponent implements OnInit{
+    @Input() text: ButtonText;
+    @Input() size: ElementSize;
+    @Input() buttonPosition: ButtonPosition;
+    @HostBinding('style.align-self') alignSelf: string;
+    constructor() {
+        console.log('SsButtonComponent loaded');
+    }
+    ngOnInit() {
+        this.setAlignSelf();
+    }
+
+    private setAlignSelf(): void {
+        switch (this.buttonPosition) {
+            case ButtonPosition.START:
+                this.alignSelf = ButtonPosition.START;
+                break;
+            case ButtonPosition.END:
+                this.alignSelf = ButtonPosition.END;
+                break;
+            case ButtonPosition.CENTER:
+                this.alignSelf = ButtonPosition.CENTER;
+                break;
+            default:
+                this.alignSelf = ButtonPosition.INHERIT;
+        }
+    }
+}
