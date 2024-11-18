87d34873f975918484adb06cf3d7a9ff084b44b7
theoliverlear <sigwartholiver2@gmail.com>
2024-11-17 20:53:37 -0600

    Add SyntaxHighlightDirective to directives.ts to inject syntax highlighting functionality into the Angular application

--- angular/directives/directives.ts
+++ angular/directives/directives.ts
@@ -1,7 +1,9 @@
 import {UndraggableDirective} from "./undraggable.directive";
 import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
+import {SyntaxHighlightDirective} from "./syntax-highlight.directive";
 
 export const directives = [
+    SyntaxHighlightDirective,
     UndraggableDirective,
     UnoptimizedImageDirective
 ];
