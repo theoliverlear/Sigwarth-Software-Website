042c187d48dccecb227f1826cdbc6614ff558b4f
theoliverlear <sigwartholiver2@gmail.com>
2025-04-25 16:24:05 -0500

    Add animationProperties.ts to define reusable animation property configurations for Angular components

---
+++ angular/components/animations/animationProperties.ts
@@ -0,0 +1,14 @@
+import {FadeInProperties} from "./models/FadeInProperties";
+
+export const quickFadeInAnimationProperties: FadeInProperties = {
+    duration: '300ms',
+    delay: '0ms'
+};
+export const nineSecondDelayMediumFadeInAnimationProperties: FadeInProperties = {
+    duration: '1500ms',
+    delay: '9000ms'
+}
+export const defaultFadeInAnimationProperties: FadeInProperties = {
+    duration: '300ms',
+    delay: '0ms'
+};
