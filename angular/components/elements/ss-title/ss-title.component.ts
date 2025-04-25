4b68bce529b6c45c6c0ed85886d95335753982a4
theoliverlear <sigwartholiver2@gmail.com>
2025-04-25 16:10:41 -0500

    Add ss-title.component.ts to be an Angular component for the ss-title element in the Angular application

---
+++ angular/components/elements/ss-title/ss-title.component.ts
@@ -0,0 +1,18 @@
+// ss-title.component.ts 
+import {Component, Input} from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+
+@Component({
+    selector: 'ss-title',
+    templateUrl: './ss-title.component.html',
+    styleUrls: ['./ss-title.component.css']
+})
+export class SsTitleComponent {
+    @Input() tagType: TagType = TagType.H1;
+    @Input() text: string;
+    constructor() {
+        
+    }
+
+    protected readonly TagType = TagType;
+}
