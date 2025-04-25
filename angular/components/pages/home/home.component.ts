7090be118906b126cffa97ee0cada9dc90d6e741
theoliverlear <sigwartholiver2@gmail.com>
2025-04-25 16:42:47 -0500

    Update stylesheet file name in home.component.ts

--- angular/components/pages/home/home.component.ts
+++ angular/components/pages/home/home.component.ts
@@ -4,7 +4,7 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home-style.component.css']
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
     constructor() {
