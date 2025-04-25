1ffe3a21b9ae24a1f0e02c4cea5c62a5525f6c56
theoliverlear <sigwartholiver2@gmail.com>
2025-04-25 16:47:35 -0500

    Remove HashPasswordService from services.ts as it is no longer used

--- angular/services/services.ts
+++ angular/services/services.ts
@@ -1,9 +1,7 @@
 import {SwipeService} from "./swipe.service";
-import {HashPasswordService} from "./hash-password.service";
 import {ErrorHandlerService} from "./error-handler.service";
 
 export const services: any[] = [
     ErrorHandlerService,
-    HashPasswordService,
     SwipeService
 ];
