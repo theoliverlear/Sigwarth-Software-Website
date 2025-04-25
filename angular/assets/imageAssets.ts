683818f8a0b895712ca5f345506919e7b0188635
theoliverlear <sigwartholiver2@gmail.com>
2025-04-25 16:19:34 -0500

    Add new image assets for plus and home icons in imageAssets.ts

--- angular/assets/imageAssets.ts
+++ angular/assets/imageAssets.ts
@@ -16,3 +16,16 @@ export function getIconImagePath(fileName: string): string {
 const imageAssetPath: string = 'assets/images/';
 const logoImageAssetPath: string = imageAssetPath + 'logo/';
 const iconImageAssetPath: string = imageAssetPath + 'icon/';
+
+export const plusIconImageAsset: ImageAsset = {
+    src: getIconImagePath('plus_icon.svg'),
+    alt: 'Plus Icon'
+};
+export const whitePlusIconImageAsset: ImageAsset = {
+    src: getIconImagePath('white_plus_icon.svg'),
+    alt: 'White Plus Icon'
+};
+export const homeIconImageAsset: ImageAsset = {
+    src: getIconImagePath('home_icon.svg'),
+    alt: 'Home Icon'
+}
