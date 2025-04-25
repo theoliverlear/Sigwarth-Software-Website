b3ea1e0d46d1152b1bbddf92aaa97078335194c8
theoliverlear <sigwartholiver2@gmail.com>
2025-04-25 16:09:20 -0500

    Add nav-item.component.ts to be an Angular component for the nav-item element in the Angular application

---
+++ angular/components/elements/nav-item/nav-item.component.ts
@@ -0,0 +1,22 @@
+// nav-item.component.ts 
+import {Component, HostListener, Input, ViewChild} from "@angular/core";
+import {TextElementLink} from "../../../models/link/TextElementLink";
+import {SsAnchorComponent} from "../ss-anchor/ss-anchor.component";
+import {navBarHomeElementLink} from "../../../assets/elementLinkAssets";
+
+@Component({
+    selector: 'nav-item',
+    templateUrl: './nav-item.component.html',
+    styleUrls: ['./nav-item.component.css']
+})
+export class NavItemComponent {
+    @Input() elementLink: TextElementLink;
+    @ViewChild(SsAnchorComponent) anchorComponent: SsAnchorComponent;
+    constructor() {
+
+    }
+    @HostListener('click')
+    onClick() {
+        this.anchorComponent.onClick();
+    }
+}
