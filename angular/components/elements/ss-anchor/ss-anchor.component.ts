7bc51e5a1e1a446420442f735ea164766ec79f96
theoliverlear <sigwartholiver2@gmail.com>
2025-04-25 16:34:11 -0500

    Expand ss-anchor.component.ts to handle internal and external link navigation with `Router` and `HostListener` integration

--- angular/components/elements/ss-anchor/ss-anchor.component.ts
+++ angular/components/elements/ss-anchor/ss-anchor.component.ts
@@ -1,9 +1,11 @@
 import {
-    Component,
+    Component, HostListener,
     Input, OnInit
 } from "@angular/core";
 import {TagType} from "../../../models/html/TagType";
 import {TextElementLink} from "../../../models/link/TextElementLink";
+import {ElementLink} from "../../../models/link/ElementLink";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'ss-anchor',
@@ -11,16 +13,27 @@ import {TextElementLink} from "../../../models/link/TextElementLink";
     styleUrls: ['./ss-anchor.component.css']
 })
 export class SsAnchorComponent implements OnInit {
-    @Input() elementLink: TextElementLink;
-    hasContent: boolean = false;
-    constructor() {
-        console.log('SsAnchorComponent loaded');
+    @Input() elementLink: TextElementLink | ElementLink;
+    hasText: boolean = false;
+    constructor(private router: Router) {
+
     }
     ngOnInit() {
-        if (this.elementLink) {
-            this.hasContent = !this.elementLink.hasText();
+        this.hasText = this.elementLink instanceof TextElementLink;
+    }
+    @HostListener('click', ['$event'])
+    onClick() {
+        if (this.isExternalLink(this.elementLink.hrefLink)) {
+            event.preventDefault();
+            window.open(this.elementLink.hrefLink, this.elementLink.targetType);
+        } else {
+            this.router.navigate([this.elementLink.hrefLink]);
         }
     }
-
+    isExternalLink(href: string): boolean {
+        return href.startsWith('http://') ||
+            href.startsWith('https://') ||
+            href.startsWith('//');
+    }
     protected readonly TagType = TagType;
 }
